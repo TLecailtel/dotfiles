f8ec6ccfcb478bf58f80037fe317823053dd1523
TLecailtel <25458141+TLecailtel@users.noreply.github.com>
2021-02-28 18:55:53 +0100

    Add: Kubernetes installation

--- install.sh
+++ install.sh
@@ -65,6 +65,12 @@ bash $MINICONDA_INSTALLER
 rm $MINICONDA_INSTALLER
 
 
+# Install Kubernetes
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+
 # Install k9s
 pacman -Syu k9s
 
