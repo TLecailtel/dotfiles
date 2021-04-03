cb135d3445bb4188e033cd8acb19ad6441e701c5
TLecailtel <25458141+TLecailtel@users.noreply.github.com>
2021-04-03 14:24:57 +0200

    Add: yay and ripgrep-all installation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 chsh -s /bin/zsh
 
 
-# Install Prezto
+# Install Prezto (zsh config framework)
 zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
@@ -11,70 +11,78 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 
 
-# Install VS Code
+# Install VS Code (IDE)
 snap install code --classic
 
 
-# Install Vim and VimPlug
+# Install Vim and VimPlug (text editor and a plugin manager)
 sudo pacman -Syu gvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-# Install bat
+# Install bat (cat alternative)
 sudo pacman -Syu bat
 
 
-# Install colordiff
+# Install colordiff (diff alternative)
 sudo pacman -Syu colordiff
 
 
-# Install jq
+# Install jq (JSON processor)
 sudo pacman -Syu jq
 
 
-# Install Rust
+# Install Rust (programming language)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup component add rls rust-analysis rust-src
 
 
-# Install tldr
+# Install tldr (man alternative)
 cargo install tealdeer
 
 
-# Install ripgrep
+# Install ripgrep (grep alternative)
 sudo pacman -Syu ripgrep
 
 
-# Install fd
+# Install yay (AUR helper)
+sudo pacman -Syu yay
+
+
+# Install ripgrep-all (ripgrep for more filetypes)
+yay -S ripgrep-all
+
+
+# Install fd (find alternative)
 sudo pacman -Syu fd
 
 
-# Install Python with Miniconda
+# Install Python with Miniconda (Python environment manager)
 MINICONDA_INSTALLER='Miniconda3-latest-Linux-x86_64.sh'
 wget "https://repo.anaconda.com/miniconda/$MINICONDA_INSTALLER"
 bash $MINICONDA_INSTALLER
 rm $MINICONDA_INSTALLER
 
 
-# Install JupyterLab
+# Install JupyterLab (notebook IDE)
 conda install -c conda-forge jupyterlab
 
 
-# Install Docker, start it on boot, and make the docker command usable as a non-root user (dangerous)
+# Install Docker (container manager), start it on boot, and make the docker command usable as a non-root user (dangerous)
 sudo pacman -Syu docker
 sudo systemctl enable docker.service
 sudo gpasswd -a tlecailtel docker
 
 
-# Install Kubernetes
+# Install Kubernetes (container cluster manager)
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 
-# Install k9s
+# Install k9s (Kubernetes UI)
 sudo pacman -Syu k9s
 
 
-# Install kubectl prompt
+# Install kubectl prompt (Kubernetes context in prompt)
 curl -O https://raw.githubusercontent.com/superbrothers/zsh-kubectl-prompt/master/kubectl.zsh
